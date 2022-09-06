e059f1c7aaaabcc093417f9929affe6b0649c28f
kuma <kuma@augusta-lab.co.jp>
2022-09-06 13:36:28 +0900

    Python版summary_countの別解

---
+++ Python/summary_count_new.py
@@ -0,0 +1,12 @@
+def calc_total(datas: list) -> int:
+    total = 0
+
+    for data in datas:
+        total += data
+
+    return total
+
+
+datas = [54, 20, 88, 30, 71, 29, 44, 9, 100, 31]
+total = calc_total(datas)
+print("合計=%d" % sum)
